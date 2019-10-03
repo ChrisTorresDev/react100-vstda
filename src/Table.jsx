a5833ad2f70deb714670465b9a3edf3a51262e10
Chris Torres <hwct83@gmail.com>
2019-10-03 16:51:43 -0700

    added updateItem form and deleteRow function

--- src/Table.jsx
+++ src/Table.jsx
@@ -1,10 +1,32 @@
 import React, { Component } from 'react';
+import UpdateItem from './UpdateItem';
 import './App';
 
 class Table extends React.Component {
-  render() {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: []
+    }
+    this.handleDeleteRow = this.handleDeleteRow.bind(this)
+  };
 
+
+  
+
+  handleDeleteRow(i) {
+    let items = [...this.state.items]
+    items.splice(i, 1)
+    this.setState({ 
+      items: items
+    })
+  }
+
+  render() {
     const items = this.props.items;
+    const updateItems = this.props.updateItems;
+
+
     return (
       <div className="card">
         <div className="card-header">
@@ -14,17 +36,13 @@ class Table extends React.Component {
           <div id="Table">
             <table>
               <tbody>
-                <tr>
-                  <th>Todo</th>
-                  <th>Priority</th>
-                </tr>
-                {items.map(item => {
+                {items.map((item, i) => {
                   return (
-                    <tr>
-                      <td><input type="checkbox" aria-label="checkbox for todo item"/></td>
-                      <td>{item.todo}</td>
-                      <td><a href="#" className="delete-todo"><i className='fas fa-trash-alt'></i></a></td>
-                      <td><a href="#" className="edit-todo"><i className='far fa-edit'></i></a></td>
+                    <tr className="alert alert-danger" role="alert" key={i}>
+                      <td className="todo-checkbox"><input type="checkbox" aria-label="checkbox for todo item"/></td>
+                      <td className='todo-item'>{item.todo}</td>
+                      <td><a className="edit-todo"><i className='far fa-edit'></i></a></td>
+                      <td><a className="delete-todo"><i className='fas fa-trash-alt' onClick={i => this.handleDeleteRow(i)}></i></a></td>
                     </tr>
                   );
                 })}
